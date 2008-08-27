de6a01f29797eebe5381786ad07ae0f21a791b2a
Aman Gupta <aman@tmm1.net>
2008-08-27 16:11:58 -0700

    store references to IO objects to prevent "Bad file descriptor - (Errno::EBADF)"

--- lib/mysqlplus.rb
+++ lib/mysqlplus.rb
@@ -3,7 +3,7 @@ require 'mysql'
 class Mysql
   def async_query(sql)
     send_query(sql)
-    select([IO.new(socket)],nil,nil,nil)
+    select [ (@sockets ||= {})[socket] ||= IO.new(socket) ], nil, nil, nil
     get_result
   end
 end
