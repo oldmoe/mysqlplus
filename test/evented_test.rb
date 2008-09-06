26202b3ddbf8f4f2be04a7c75428e4f5ca169f2f
Lourens Naude <lourens@methodmissing.com>
2008-09-06 17:21:36 +0100

    Assume blank password

--- test/evented_test.rb
+++ test/evented_test.rb
@@ -1,12 +1,12 @@
 require File.dirname(__FILE__) + '/test_helper'
 
 EventedMysqlTest.new( 10 ) do |test|
-  test.setup{ Mysql.real_connect('localhost','root','3421260') }
+  test.setup{ Mysql.real_connect('localhost','root') }
   test.run!
 end
 
 EventedMysqlTest.new( 10 ) do |test|
-  test.setup{ Mysql.real_connect('localhost','root','3421260') }
+  test.setup{ Mysql.real_connect('localhost','root') }
   test.c_async_query = true
   test.run!
 end
