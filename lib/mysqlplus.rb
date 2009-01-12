edff42ab2cbba947ea0c8302cc64ba2fae0446e2
unknown <PackRD@.(none)>
2009-01-12 11:18:33 -0700

    use c_async_query by default

--- lib/mysqlplus.rb
+++ lib/mysqlplus.rb
@@ -2,11 +2,13 @@ require 'mysql'
 
 class Mysql
   
-  def async_query(sql, timeout = nil)
+  def ruby_async_query(sql, timeout = nil) # known to deadlock TODO
     send_query(sql)
     select [ (@sockets ||= {})[socket] ||= IO.new(socket) ], nil, nil, nil
     get_result
   end
+
+  alias_method :async_query, :c_async_query  
   
 end
 
