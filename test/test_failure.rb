bb63b9d78c8245ce2736e736765edfe9dc34cbe6
Aman Gupta <aman@tmm1.net>
2009-01-25 12:59:25 -0800

    remove misleading comments from tests

--- test/test_failure.rb
+++ test/test_failure.rb
@@ -1,7 +1,3 @@
-# shows the effect of using .all_hashes instead of looping on each hash 
-# run it by substiting in a 'long' [many row] query for the query variable and toggling use_all_hashes here at the top
-# note that we load all the rows first, then run .all_hashes on the result [to see more easily the effect of all hashes]
-# on my machine and a 200_000 row table, it took 3.38s versus 3.65s
 require 'rubygems'
 require 'mysqlplus'
 begin
