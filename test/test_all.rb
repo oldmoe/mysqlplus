021cd366702265b81bb88964bc709b5a1a4d8597
Roger Pack <rogerpack2005@gmail.com>
2009-04-18 23:06:12 +0000

    add test runner for all

---
+++ test/test_all.rb
@@ -0,0 +1,9 @@
+# I suppose if all the tests don't blow up, that probably means pass
+require 'mysqlplus'
+for file in Dir.glob('*_test.rb') do
+ puts 'testing ' + file + "\n\n\n\n\n"
+ # fork so we don't run out of connections to the mysql db, as few tests ever clean up their old processes
+ pid = Process.fork { load file }
+ Process.wait(pid)
+end
+puts 'successful'
