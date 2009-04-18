49726a5d80fd65dde237d23ad0db802a38a3654c
Roger Pack <rogerpack2005@gmail.com>
2009-04-18 23:07:30 +0000

    minor cleanup

--- test/test_all.rb
+++ test/test_all.rb
@@ -1,7 +1,7 @@
 # I suppose if all the tests don't blow up, that probably means pass
 require 'mysqlplus'
 for file in Dir.glob('*_test.rb') do
- puts 'testing ' + file + "\n\n\n\n\n"
+ puts 'testing ' + file
  # fork so we don't run out of connections to the mysql db, as few tests ever clean up their old processes
  pid = Process.fork { load file }
  Process.wait(pid)
