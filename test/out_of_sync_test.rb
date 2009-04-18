0f14fb920cb89ae59cf55da1abbd1c53d5f7b7ad
Roger Pack <rogerpack2005@gmail.com>
2009-04-18 22:42:12 +0000

    add an assertion for the out_of_sync_test

--- test/out_of_sync_test.rb
+++ test/out_of_sync_test.rb
@@ -2,13 +2,13 @@ require File.dirname(__FILE__) + '/test_helper'
 
 m = Mysql.real_connect('localhost','root')
 m.reconnect = true
-
+$count = 0
 class << m
-  
   def safe_query( query )
     begin
       send_query( query )
     rescue => e
+      $count += 1
       puts e.message
     end
   end
@@ -25,7 +25,10 @@ m.connect('localhost','root')
 m.safe_query( 'select sleep(1)' )
 m.safe_query( 'select sleep(1)' )#raises
 m.simulate_disconnect
+raise unless $count == 3
 m.safe_query( 'BEGIN' )
-m.safe_query( 'select sleep(1)' )
+m.safe_query( 'select sleep(1)' ) # raises
 m.get_result()
 m.safe_query( 'COMMIT' )
+m.get_result
+raise unless $count == 4
