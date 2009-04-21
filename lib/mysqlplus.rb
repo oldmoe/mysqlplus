52d95ff3e8516c59baceebaf4e338cc001ace750
Roger Pack <rogerpack2005@gmail.com>
2009-04-21 01:41:06 +0000

    better exception message on collision with mysql gem

--- lib/mysqlplus.rb
+++ lib/mysqlplus.rb
@@ -15,7 +15,7 @@ class Mysql
   begin
     alias_method :async_query, :c_async_query  
   rescue NameError => e
-    raise LoadError.new("error loading mysqlplus--this may mean you ran a require 'mysql' before a require 'mysqplus', which must come first")
+    raise LoadError.new("error loading mysqlplus--this may mean you ran a require 'mysql' before a require 'mysqplus', which must come first -- possibly also run gem uninstall mysql")
   end
   
 end
